3fd80984a9e859138a6ffd6cf3a0e70e7d381067
kinyua-kahoro <kinyuaa.kahoro@gmail.com>
2025-03-04 10:10:54 +0300

    crud operations

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from myapp import views
 urlpatterns = [
-
+    path('', views.user_list),
+    path('Add/', views.add_user),
+    path('Edit/<id>/', views.Edituser),
+    path('Delete/<eid>/', views.Deleteuser),
+    path('View/<eid>/', views.Viewuser),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User
+from .forms import UserForm
+
 
 # Create your views here.
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'listing_page.html', {'users':users})
+
+def add_user(request):
+    mydict={}
+    form = UserForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict['form'] = form
+    return render(request, 'add.html', mydict)
+
+def Edituser(request,id=None):
+    one_record = User.objects.get(pk=id)
+    form = UserForm(request.POST or None, request.FILES or None, instance=one_record)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict={'form':form}
+    return render(request, 'edit.html', context=mydict)
+
+def Deleteuser(request,eid=None):
+    one_record = User.objects.get(pk=eid)
+    if request.method == 'POST':
+        one_record.delete()
+        return redirect('/')
+    return render(request, 'delete.html')
+
+def Viewuser(request,eid=None):
+    mydict={}
+    one_record = User.objects.get(pk=eid)
+    mydict['user'] = one_record
+    return render(request, 'views.html', mydict)
